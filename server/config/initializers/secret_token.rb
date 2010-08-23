e484d35d34f646f185dc975eb37715511421739d
Eduardo <edu@kubuntu-vm.(none)>
2010-08-23 23:10:51 +0200

    Added server app

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '81166e7f874f51c85053a5dc8f83a9f72e0283b149f7a25b6215ee430a0953d9b1f29cacd72c4eafcde7bf195f462f8fdc021cb8455c541ee6a8a0d71e5bfaac'
